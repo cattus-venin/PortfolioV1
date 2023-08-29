b866f162ca6e24e1eb1d348f1810d54a5672c42c
cattus-venin <123291895+cattus-venin@users.noreply.github.com>
2023-08-29 16:27:07 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const Navbar = () => {
                 </a>
                 <a
                   className="text-gray-500 hover:text-transparent bg-clip-text bg-gradient-to-r to-emerald-300 from-purple-400 hover:bg-green-300 px-3 py-2 rounded-md text-xl font-extrabold font-poppins hover:bg-gradient-to-r to-emerald-300 from-purple-400">
-                  <Link to="/Experience"} >Experiences</Link>
+                  <Link to="/Experience">Experiences</Link>
                 </a>
                 <a
                   className="text-gray-500 hover:text-white px-3 py-2 rounded-md text-xl font-extrabold font-poppins">
